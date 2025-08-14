0dcf2b07f399ad90ad0c54cd8c9f932b14459aef
Cronan99 <albincronmark@gmail.com>
2025-08-14 11:47:15 +0200

    Added model training

--- main.py
+++ main.py
@@ -5,19 +5,58 @@ import os
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tqdm import tqdm
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
 
 #%%
 
-def load_frozen_graph(pb_file_path):
-    with tf.io.gfile.GFile(pb_file_path, "rb") as f:
-        graph_def = tf.compat.v1.Graphdef()
-        graph_def.ParseFromString(f.read())
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
 
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(graph_def, name="")
-    return graph
+datagen = ImageDataGenerator(rescale=1./255)
 
-graph = load_frozen_graph(".model/saved_model.pb")
+train_gen = datagen.flow_from_directory(
+    ".data/Human Bone Fractures Multi-modal Image Dataset (HBFMID)/Bone Fractures Detection/train",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    shuffle=True
+)
 
+valid_gen = datagen.flow_from_directory(
+    ".data/Human Bone Fractures Multi-modal Image Dataset (HBFMID)/Bone Fractures Detection/valid",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    shuffle=False
+)
+
+test_gen = datagen.flow_from_directory(
+    ".data/Human Bone Fractures Multi-modal Image Dataset (HBFMID)/Bone Fractures Detection/test",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    shuffle=False
+)
 #%%
 
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(2, activation='softmax')(x)  
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer=Adam(1e-3), loss="categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_gen, validation_data=valid_gen, epochs=5)
+
+
+# %%
